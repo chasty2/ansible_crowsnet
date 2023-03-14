347448fc7e89602f5a84f2b99ca58dbf4a903bf8
chasty2 <email@codyhasty.net>
2023-03-13 19:23:14 -0500

    updated comments of run script

--- run-ansible.sh
+++ run-ansible.sh
@@ -3,17 +3,17 @@
 ## script to run ansible container with a given playbook
 ## Written by Cody Hasty
 
-# Example use: ./run_ansible.sh playbook_file
+# Example use: ./run_ansible.sh playbook.yml [--tags tag_name]
 
 #
 ## Arguments to podman
 #
-# -it							interactive mode 
-# --rm							delete container after use
-# --network host					use IP address of host VM
-#--volume PATH/configs:/etc/ansible 			point /etc/ansible in container to configs directory in host
-# -w /etc/ansible 					set /etc/ansible as working directory inside container
-# ansible_crowsnet					specify container
-# $1 							specify a playbook at command line when running this script              
+# -it							            interactive mode 
+# --rm							            delete container after use
+# --network host					        use IP address of host VM
+#--volume PATH/configs:/etc/ansible 		point /etc/ansible in container to configs directory in host
+# -w /etc/ansible 					        set /etc/ansible as working directory inside container
+# ansible_crowsnet					        specify container
+# $@ 							            specify a playbook at command line when running this script (and optionally tags, etc)           
 
 podman run -it --rm --network host --volume /home/chasty2/Documents/ansible_crowsnet/configs:/etc/ansible -w /etc/ansible ansible-crowsnet $@
