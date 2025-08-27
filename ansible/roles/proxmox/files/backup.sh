b412f053265c4dcb3b7d2742d3bea6d0e6be4618
chasty2 <email@codyhasty.net>
2025-08-27 16:46:45 -0500

    Remove unused backup script

--- ansible/roles/proxmox/files/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Backup file system to Proxmox Backup Server using proxmox-backup-client
-# Written by Cody Hasty
-#
-# Usage: backup.sh namespace file_system
-proxmox-backup-client backup $1.pxar:$2 --repository root@pam@192.168.4.12:backup --ns $1
