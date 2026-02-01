c4650cd4cdac95e33ea67c69cac73858f4707237
chasty2 <email@codyhasty.net>
2026-02-01 11:35:00 -0600

    fix: Allow ansible options to pass through CLI commands
    Add context_settings with ignore_unknown_options and UNPROCESSED type
    to allow options like --limit and --check to pass through to
    ansible-playbook instead of being interpreted by Click.

--- crowsnet.py
+++ crowsnet.py
@@ -11,44 +11,44 @@ def cli():
     """CrowsNet infrastructure management CLI."""
 
 
-@cli.command()
-@click.argument("extra_args", nargs=-1)
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("extra_args", nargs=-1, type=click.UNPROCESSED)
 def build(extra_args):
     """Build the ansible container."""
     sys.exit(build_container(list(extra_args) if extra_args else None))
 
 
-@cli.command()
+@cli.command(context_settings={"ignore_unknown_options": True})
 @click.argument("playbook")
-@click.argument("extra_args", nargs=-1)
+@click.argument("extra_args", nargs=-1, type=click.UNPROCESSED)
 def run(playbook, extra_args):
     """Run a custom ansible playbook."""
     sys.exit(run_playbook(playbook, list(extra_args) if extra_args else None))
 
 
-@cli.command()
-@click.argument("extra_args", nargs=-1)
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("extra_args", nargs=-1, type=click.UNPROCESSED)
 def site(extra_args):
     """Run the full site deployment (site.yml)."""
     sys.exit(run_playbook("site.yml", list(extra_args) if extra_args else None))
 
 
-@cli.command()
-@click.argument("extra_args", nargs=-1)
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("extra_args", nargs=-1, type=click.UNPROCESSED)
 def physical(extra_args):
     """Run physical hosts deployment (physical.yml)."""
     sys.exit(run_playbook("physical.yml", list(extra_args) if extra_args else None))
 
 
-@cli.command()
-@click.argument("extra_args", nargs=-1)
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("extra_args", nargs=-1, type=click.UNPROCESSED)
 def virtual(extra_args):
     """Run virtual hosts deployment (virtual.yml)."""
     sys.exit(run_playbook("virtual.yml", list(extra_args) if extra_args else None))
 
 
-@cli.command()
-@click.argument("extra_args", nargs=-1)
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("extra_args", nargs=-1, type=click.UNPROCESSED)
 def update(extra_args):
     """Update and reboot all VMs (update.yml)."""
     sys.exit(run_playbook("update.yml", list(extra_args) if extra_args else None))
